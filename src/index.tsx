8463d5d58d39a608b3e124334d92858055357d46
Johan R-G <johan@usercise.com>
2018-05-04 14:49:07 +0100

    Add react-route dependency, render template name for routes in state

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,32 @@
 import * as React from "react";
-import { VocabCMSProps, VocabCMSState, Route } from "./interface";
+import { Route, Switch } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
+import { IVocabCMSProps, IVocabCMSState, IRoute } from "./interface";
 
-export default class VocabCMS extends React.Component<VocabCMSProps, VocabCMSState> {
-    constructor(props: VocabCMSProps) {
+export default class VocabCMS extends React.Component<IVocabCMSProps, IVocabCMSState> {
+    constructor(props: IVocabCMSProps) {
         super(props);
-        const emptyRoute: Array<Route> = [];
+        const emptyRoute: Array<IRoute> = [];
         this.state = {
             routes: props.initialState.routes || emptyRoute,
         }
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    {this.state.routes.map(
+                        (route, i) => 
+                            <Route key={i} 
+                                    path={route.path} 
+                                    exact={route.exact}
+                                    render={props => (
+                                        <p>{route.template}</p>
+                                    )}
+                    />)}
+                </Switch>
+            </Router>
+        );
     }
 }
--- src/interface.ts
+++ src/interface.ts
@@ -1,34 +1,34 @@
 
-export interface Map<T> {
+export interface IMap<T> {
     [key: string]: T;
 }
 
-export interface Route {
+export interface IRoute {
     path: string;
     template: string;
     props: {
         meta?: any;
         title?: string;
-        slots: Map<Slot>;
+        slots: IMap<ISlot>;
     }
 }
 
-export interface Slot {
+export interface ISlot {
     id: string;
     global: boolean;
-    components: Array<Component>
+    components: Array<IComponent>
 }
 
-export interface Component {
+export interface IComponent {
     id: string;
     component: string;
-    props: Map<any>;
+    props: IMap<any>;
 }
 
-export interface VocabCMSState { routes: Array<Route>; }
+export interface IVocabCMSState { routes: Array<IRoute>; }
 
-export interface VocabCMSProps { 
+export interface IVocabCMSProps { 
     readonly initialState?: { 
-        routes: Array<Route>;
+        routes: Array<IRoute>;
     };
 }
