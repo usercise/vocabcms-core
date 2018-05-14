30a2621ba5c6f2db91f428655d671e6344e85743
Johan R-G <johan@usercise.com>
2018-05-14 21:39:52 +0100

    Clean up loading screen component and attachment

--- src/VocabCMS.tsx
+++ src/VocabCMS.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Route, Switch } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
 import { IVocabCMSProps, IVocabCMSState, IRoute } from "./interface";
+import LoadingScreen from './template/LoadingScreen';
 
 export default class VocabCMS extends React.Component<IVocabCMSProps, IVocabCMSState> {
     constructor(props: IVocabCMSProps) {
@@ -38,6 +39,9 @@ export default class VocabCMS extends React.Component<IVocabCMSProps, IVocabCMSS
                                         <p>{route.template}</p>
                                     )}
                     />)}
+                    <Route key="loadingScreen" render={({ match }) =>
+                      <LoadingScreen pageLookupServicelUrl="/_reserved/api/omnibus" addRoute={this.addRoute.bind(this)} match={match} />
+                    } />
                 </Switch>
             </Router>
         );
